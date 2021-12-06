170a54e973ba62719928e0b3901b55e1bc057c5d
Paul Woitaschek <woitaschek@gmail.com>
2021-12-06 08:28:11 +0100

    Set up the structure for Day6.

---
+++ src/main/kotlin/Day6.kt
@@ -0,0 +1,14 @@
+@file:Suppress("ConvertCallChainIntoSequence")
+
+object Day6 : Puzzle {
+
+  override val day = 6
+
+  override fun solvePart1(input: String): Int {
+    return 42
+  }
+
+  override fun solvePart2(input: String): Int {
+    return 42
+  }
+}
