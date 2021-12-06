a35778abbb8b84cb907310a80c93a15e39e802ae
Paul Woitaschek <woitaschek@gmail.com>
2021-12-06 08:37:31 +0100

    Solve part1 of Day6.

--- src/main/kotlin/Day6.kt
+++ src/main/kotlin/Day6.kt
@@ -5,10 +5,29 @@ object Day6 : Puzzle {
   override val day = 6
 
   override fun solvePart1(input: String): Int {
-    return 42
+    return generateSequence(Ocean.parse(input)) { it.afterOneDay() }
+      .take(81).last().totalFishes
   }
 
   override fun solvePart2(input: String): Int {
     return 42
+  }
+}
+
+private data class Ocean(private val breedTimes: List<Int>) {
+
+  val totalFishes: Int = breedTimes.size
+
+  fun afterOneDay(): Ocean {
+    val agedFishes = breedTimes.map { if (it == 0) 6 else it - 1 }
+    val bornFishes = List(breedTimes.count { it == 0 }) { 8 }
+    return Ocean(agedFishes + bornFishes)
+  }
+
+  companion object {
+    fun parse(input: String): Ocean {
+      val fishes = input.lines().single().split(",").map(String::toInt)
+      return Ocean(fishes)
+    }
   }
 }
