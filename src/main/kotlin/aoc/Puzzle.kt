960b1628a2e080869bc32a55a960ae5c44dfbf01
Paul Woitaschek <woitaschek@gmail.com>
2021-12-13 16:59:41 +0100

    Solve day 13.

--- src/main/kotlin/aoc/Puzzle.kt
+++ src/main/kotlin/aoc/Puzzle.kt
@@ -4,7 +4,7 @@ interface Puzzle {
 
   val day: Int
 
-  fun solvePart1(input: String): Long
+  fun solvePart1(input: String): Any
 
-  fun solvePart2(input: String): Long
+  fun solvePart2(input: String): Any
 }
