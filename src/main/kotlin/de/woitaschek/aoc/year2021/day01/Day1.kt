34a65fa548a037a4125f681537e805660087631d
Paul Woitaschek <woitaschek@gmail.com>
2022-11-28 09:24:45 +0100

    Prepare the repo to host multiple years.

---
+++ src/main/kotlin/de/woitaschek/aoc/year2021/day01/Day1.kt
@@ -0,0 +1,30 @@
+@file:Suppress("ConvertCallChainIntoSequence")
+
+package de.woitaschek.aoc.year2021.day01
+
+import de.woitaschek.aoc.year2021.Puzzle
+
+object Day1 : Puzzle {
+
+  override val day = 1
+
+  override fun solvePart1(input: String): Long {
+    return input.lines().map { it.toInt() }
+      .windowed(2)
+      .count { (previous, current) ->
+        current > previous
+      }
+      .toLong()
+  }
+
+  override fun solvePart2(input: String): Long {
+    return input.lines().map { it.toInt() }
+      .windowed(size = 3)
+      .map { it.sum() }
+      .windowed(2)
+      .count { (previous, current) ->
+        current > previous
+      }
+      .toLong()
+  }
+}
