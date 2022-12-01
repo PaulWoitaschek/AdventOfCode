b385252b0574f21c879109501a9851b56969bfad
Paul Woitaschek <woitaschek@gmail.com>
2022-12-01 10:37:37 +0100

    Simplify the Day1 solution.

--- src/main/kotlin/de/woitaschek/aoc/year2022/Day1.kt
+++ src/main/kotlin/de/woitaschek/aoc/year2022/Day1.kt
@@ -3,25 +3,16 @@ package de.woitaschek.aoc.year2022
 import de.woitaschek.aoc.Puzzle
 
 object Day1 : Puzzle {
-
   override val day: Int = 1
-
   override fun solvePart1(input: String) = parse(input).max()
-
   override fun solvePart2(input: String) = parse(input).sortedDescending().take(3).sum()
 }
 
 private fun parse(input: String): List<Long> {
-  val elves = mutableListOf<Long>()
-  val lines = input.lines()
-  var calories = 0L
-  lines.forEachIndexed { index, line ->
-    if (line.isBlank() || index == lines.size - 1) {
-      elves.add(calories)
-      calories = 0
-    } else {
-      calories += line.toLong()
+  return input.split("\n\n")
+    .map { elv ->
+      elv.split("\n").sumOf { line ->
+        line.toLongOrNull() ?: 0L
+      }
     }
-  }
-  return elves
 }
