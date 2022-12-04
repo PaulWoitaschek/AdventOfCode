5d4fec5f46651e7db9fe6df145ed95492b7b1c4d
Paul Woitaschek <woitaschek@gmail.com>
2022-12-04 21:25:47 +0100

    Solve part 3

---
+++ src/main/kotlin/de/woitaschek/aoc/year2022/Day3.kt
@@ -0,0 +1,32 @@
+package de.woitaschek.aoc.year2022
+
+import de.woitaschek.aoc.Puzzle
+
+object Day3 : Puzzle {
+
+  override val day = 3
+
+  override fun solvePart1(input: String) = input.lines()
+    .filter(String::isNotEmpty).sumOf { line ->
+      val left = line.substring(0, line.length / 2)
+      val right = line.substring(line.length / 2, line.length)
+      left.toSet().intersect(right.toSet()).single().score()
+    }
+
+  override fun solvePart2(input: String) = input.lines()
+    .filter(String::isNotEmpty)
+    .chunked(3)
+    .sumOf {
+      it.reduce { previous, current ->
+        previous.toSet().intersect(current.toSet()).joinToString()
+      }.single().score()
+    }
+}
+
+private fun Char.score(): Int {
+  return if (isLowerCase()) {
+    this - 'a' + 1
+  } else {
+    this - 'A' + 27
+  }
+}
