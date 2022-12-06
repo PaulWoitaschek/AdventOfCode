fba77649ce3c1a6d8deab81b95fa7adb5f573ddc
Paul Woitaschek <woitaschek@gmail.com>
2022-12-06 23:07:58 +0100

    Solve Day9 of year 2020.

---
+++ src/main/kotlin/de/woitaschek/aoc/year2020/Day9.kt
@@ -0,0 +1,44 @@
+package de.woitaschek.aoc.year2020
+
+import de.woitaschek.aoc.utils.Puzzle
+
+class Day9(private val premableLength: Int) : Puzzle(2020, 9) {
+
+  override fun solvePart1(input: String) = invalidNumber(input)
+
+  override fun solvePart2(input: String): Any {
+    val invalidNumber = invalidNumber(input)
+    input.lineSequence().mapNotNull(String::toIntOrNull)
+      .windowed(invalidNumber, 1, true)
+      .forEach a@{ window ->
+        val result = mutableListOf<Int>()
+        window.forEach {
+          result.add(it)
+          val sum = result.sum()
+          if (sum == invalidNumber) {
+            return result.min() + result.max()
+          } else if (sum > invalidNumber) {
+            return@a
+          }
+        }
+      }
+    error("No secret number")
+  }
+
+  private fun invalidNumber(input: String) = input.lineSequence()
+    .mapNotNull(String::toIntOrNull)
+    .windowed(premableLength + 1)
+    .first { !it.dropLast(1).hasSumPair(it.last()) }
+    .last()
+}
+
+private fun List<Int>.hasSumPair(sum: Int): Boolean {
+  forEachIndexed { xIndex, x ->
+    forEachIndexed { yIndex, y ->
+      if (xIndex != yIndex && x + y == sum) {
+        return true
+      }
+    }
+  }
+  return false
+}
