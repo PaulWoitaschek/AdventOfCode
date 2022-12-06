7892f33d10fe2655b88cefa0386a9c16fa7bc052
Paul Woitaschek <woitaschek@gmail.com>
2022-12-06 23:07:58 +0100

    Solve Day9 of year 2020.

--- src/test/kotlin/de/woitaschek/aoc/AocTest.kt
+++ src/test/kotlin/de/woitaschek/aoc/AocTest.kt
@@ -3,17 +3,29 @@ package de.woitaschek.aoc
 import de.woitaschek.aoc.utils.Puzzle
 import io.kotest.matchers.shouldBe
 
-fun Puzzle.test(part1: Any?, part2: Any?) {
-  val input = input()
+fun Puzzle.test(
+  part1: Any? = null,
+  part2: Any? = null,
+  part1Test: Any? = null,
+  part2Test: Any? = null,
+) {
   if (part1 != null) {
-    solvePart1(input) shouldBe part1
+    solvePart1(input()) shouldBe part1
   }
   if (part2 != null) {
-    solvePart2(input) shouldBe part2
+    solvePart2(input()) shouldBe part2
+  }
+  if (part1Test != null) {
+    solvePart1(testInput()) shouldBe part1Test
+  }
+  if (part2Test != null) {
+    solvePart2(testInput()) shouldBe part2Test
   }
 }
 
 fun Puzzle.input(): String = aocInput(year = year, day = day)
+
+fun Puzzle.testInput(): String = aocTestInput(year = year, day = day)
 
 @Suppress("unused")
 fun testInput(): String = input("testinput.txt")
@@ -21,6 +33,11 @@ fun testInput(): String = input("testinput.txt")
 fun aocInput(year: Int, day: Int): String {
   return input("$year/day$day.txt")
 }
+
+fun aocTestInput(year: Int, day: Int): String {
+  return input("$year/day${day}test.txt")
+}
+
 
 private fun input(fileName: String): String {
   return ClassLoader
