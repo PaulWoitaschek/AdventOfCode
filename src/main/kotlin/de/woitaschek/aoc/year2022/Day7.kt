eace80e6a975623b2fca525c020705d8c6a70951
Paul Woitaschek <woitaschek@gmail.com>
2022-12-07 11:42:11 +0100

    Solve day 7.

---
+++ src/main/kotlin/de/woitaschek/aoc/year2022/Day7.kt
@@ -0,0 +1,61 @@
+package de.woitaschek.aoc.year2022
+
+import de.woitaschek.aoc.utils.Puzzle
+
+object Day7 : Puzzle(2022, 7) {
+
+  override fun solvePart1(input: String): Long {
+    return directorySizes(input).filter { it <= 100_000 }.sum()
+  }
+
+  override fun solvePart2(input: String): Long {
+    val directories = directorySizes(input)
+    val minSpaceToFree = directories.max() - (70_000_000 - 30_000_000)
+    return directories.first { it >= minSpaceToFree }
+  }
+
+  private data class PuzzleFile(val size: Long, val path: String)
+
+  private fun directorySizes(input: String): List<Long> {
+    val files = parseFiles(input)
+    return files.flatMap { file -> file.directories() }.toSet()
+      .map { dir ->
+        files.filter { file -> file.path.startsWith(dir) }.sumOf { it.size }
+      }
+      .sorted()
+  }
+
+  private fun PuzzleFile.directories(): MutableList<String> {
+    val filePath = path
+    val directories = mutableListOf<String>()
+    filePath.forEachIndexed { index, c ->
+      if (c == '/') {
+        directories.add(filePath.take(index + 1))
+      }
+    }
+    return directories
+  }
+
+  private fun parseFiles(input: String): Set<PuzzleFile> {
+    var currentPath = "/"
+    val files = mutableSetOf<PuzzleFile>()
+    input.lines().filter(String::isNotEmpty).forEach { line ->
+      when {
+        line.startsWith("\$ cd") -> {
+          val path = line.drop(5)
+          currentPath = when (path) {
+            ".." -> currentPath.dropLast(1).dropLastWhile { it != '/' }
+            "/" -> "/"
+            else -> "$currentPath$path/"
+          }
+        }
+
+        line.first().isDigit() -> {
+          val (size, name) = line.split(" ")
+          files.add(PuzzleFile(size = size.toLong(), path = currentPath + name))
+        }
+      }
+    }
+    return files
+  }
+}
---
+++ src/test/kotlin/de/woitaschek/aoc/year2022/Day7Test.kt
@@ -0,0 +1,17 @@
+package de.woitaschek.aoc.year2022
+
+import de.woitaschek.aoc.test
+import org.junit.jupiter.api.Test
+
+class Day7Test {
+
+  @Test
+  fun test() {
+    Day7.test(
+      part1Test = 95437,
+      part1 = 1477771,
+      part2Test = 24933642,
+      part2 = 3579501,
+    )
+  }
+}
