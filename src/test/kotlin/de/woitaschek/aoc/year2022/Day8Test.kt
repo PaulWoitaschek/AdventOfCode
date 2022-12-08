26118be12faf1f1d3a1609f68d1b666bf9d336d1
Paul Woitaschek <woitaschek@gmail.com>
2022-12-08 17:41:19 +0100

    Solve day8.

---
+++ src/test/kotlin/de/woitaschek/aoc/year2022/Day8Test.kt
@@ -0,0 +1,17 @@
+package de.woitaschek.aoc.year2022
+
+import de.woitaschek.aoc.test
+import org.junit.jupiter.api.Test
+
+class Day8Test {
+
+  @Test
+  fun test() {
+    Day8.test(
+      part1Test = 21,
+      part1 = 1672,
+      part2Test = 8,
+      part2 = 327180,
+    )
+  }
+}
