582ac2d799164edbc5c3b0858257909997e5cd12
Paul Woitaschek <woitaschek@gmail.com>
2022-12-11 22:38:07 +0100

    Solve day11.

---
+++ src/test/kotlin/de/woitaschek/aoc/year2022/Day11Test.kt
@@ -0,0 +1,17 @@
+package de.woitaschek.aoc.year2022
+
+import de.woitaschek.aoc.test
+import org.junit.jupiter.api.Test
+
+class Day11Test {
+
+  @Test
+  fun test() {
+    Day11.test(
+      part1Test = 10605,
+      part1 = 99840,
+      part2 = 20683044837,
+      part2Test = 2713310158,
+    )
+  }
+}
