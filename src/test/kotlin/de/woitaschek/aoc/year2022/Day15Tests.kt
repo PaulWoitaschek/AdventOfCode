f287d6c8a41c9ff4de629d2a13cb0008db6b38e3
Paul Woitaschek <woitaschek@gmail.com>
2022-12-15 16:10:46 +0100

    Solve day15.

---
+++ src/test/kotlin/de/woitaschek/aoc/year2022/Day15Tests.kt
@@ -0,0 +1,25 @@
+package de.woitaschek.aoc.year2022
+
+import de.woitaschek.aoc.aocInput
+import de.woitaschek.aoc.aocTestInput
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+class Day15Tests {
+
+  @Test
+  fun testData() {
+    val input = aocTestInput(2022, 15)
+    val puzzle = Day15(lineToScan = 10, rangeMax = 20)
+    puzzle.solvePart1(input) shouldBe 26
+    puzzle.solvePart2(input) shouldBe 56000011
+  }
+
+  @Test
+  fun realData() {
+    val input = aocInput(2022, 15)
+    val puzzle = Day15(lineToScan = 2000000, rangeMax = 4000000)
+    puzzle.solvePart1(input) shouldBe 5394423
+    puzzle.solvePart2(input) shouldBe 11840879211051
+  }
+}
