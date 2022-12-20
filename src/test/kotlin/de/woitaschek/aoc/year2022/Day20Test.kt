2abca149fd3ab43f4655f7c02d18dc3ed9581561
Paul Woitaschek <woitaschek@gmail.com>
2022-12-20 16:36:46 +0100

    Solve day 20.

---
+++ src/test/kotlin/de/woitaschek/aoc/year2022/Day20Test.kt
@@ -0,0 +1,17 @@
+package de.woitaschek.aoc.year2022
+
+import de.woitaschek.aoc.test
+import org.junit.jupiter.api.Test
+
+class Day20Test {
+
+  @Test
+  fun test() {
+    Day20.test(
+      part1Test = 3L,
+      part1 = 19559L,
+      part2Test = 1623178306L,
+      part2 = 912226207972L,
+    )
+  }
+}
