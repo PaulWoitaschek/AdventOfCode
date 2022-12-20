ae55a8890f2a05d4b7b22863ebd079808bcbe29d
Paul Woitaschek <woitaschek@gmail.com>
2022-12-20 07:50:54 +0100

    Solve day 18 part 1.

---
+++ src/test/kotlin/de/woitaschek/aoc/year2022/Day18Test.kt
@@ -0,0 +1,17 @@
+package de.woitaschek.aoc.year2022
+
+import de.woitaschek.aoc.test
+import org.junit.jupiter.api.Test
+
+class Day18Test {
+
+  @Test
+  fun part1TestInput() {
+    Day18.test(part1Test = 64)
+  }
+
+  @Test
+  fun part1RegularInput() {
+    Day18.test(part1 = 4370)
+  }
+}
