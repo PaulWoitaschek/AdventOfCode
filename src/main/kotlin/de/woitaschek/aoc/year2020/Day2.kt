397e7efe849c8c7b0d1c1e9b1a0f1f1d5960da85
Paul Woitaschek <woitaschek@gmail.com>
2022-12-29 20:15:57 +0100

    Solve 2020 day 2.

---
+++ src/main/kotlin/de/woitaschek/aoc/year2020/Day2.kt
@@ -0,0 +1,29 @@
+package de.woitaschek.aoc.year2020
+
+import de.woitaschek.aoc.utils.Puzzle
+
+object Day2 : Puzzle(2020, 2) {
+
+  override fun solvePart1(input: String): Int = parseInput(input).count(PasswordPolicy::isValidForPart1)
+
+  override fun solvePart2(input: String): Int = parseInput(input).count(PasswordPolicy::isValidForPart2)
+
+  private fun parseInput(input: String): List<PasswordPolicy> {
+    val regex = "(\\d+)-(\\d+) (.): (.*)".toRegex()
+    return input.lines().filter(String::isNotEmpty).map {
+      val (min, max, char, password) = regex.find(it)!!.destructured
+      PasswordPolicy(min = min.toInt(), max = max.toInt(), char = char.single(), password = password)
+    }
+  }
+
+  private data class PasswordPolicy(
+    val min: Int,
+    val max: Int,
+    val char: Char,
+    val password: String,
+  ) {
+    fun isValidForPart1(): Boolean = password.count { it == char } in min..max
+    fun isValidForPart2(): Boolean = listOf(min, max).count { password[it - 1] == char } == 1
+  }
+}
+
