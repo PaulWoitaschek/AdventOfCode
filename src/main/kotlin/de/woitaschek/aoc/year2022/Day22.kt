3c7777b3ff1c29120040c917d04066fcefa3801b
Paul Woitaschek <woitaschek@gmail.com>
2022-12-29 18:41:21 +0100

    Solve day22 part 1.

---
+++ src/main/kotlin/de/woitaschek/aoc/year2022/Day22.kt
@@ -0,0 +1,144 @@
+package de.woitaschek.aoc.year2022
+
+import de.woitaschek.aoc.utils.Point
+import de.woitaschek.aoc.utils.Puzzle
+import de.woitaschek.aoc.year2022.Day22.Facing.Right
+
+private typealias Board = List<List<Day22.Tile>>
+
+object Day22 : Puzzle(2022, 22) {
+
+  override fun solvePart1(input: String): Any {
+    val (mapString, instructions) = input.split("\n\n")
+
+    val mapLines = mapString.lines()
+    val width = mapLines.maxOf { it.count() }
+
+    val board = mapLines.map { line ->
+      List(width) {
+        when (line.getOrNull(it)) {
+          null, ' ' -> Tile.Void
+          '.' -> Tile.Floor
+          '#' -> Tile.Wall
+          else -> error("Unknown tile=$it")
+        }
+      }
+    }
+
+    val height = board.size
+
+    val result = mutableListOf<Instruction>()
+    var buffer = ""
+    val trimmedInstructions = instructions.trim().removeSurrounding("\n")
+    trimmedInstructions.forEachIndexed { index, c ->
+      val isLast = index == trimmedInstructions.lastIndex
+      when {
+        c.isDigit() -> {
+          buffer += c
+          if (isLast) {
+            result += Instruction.Move(buffer.toInt())
+          }
+        }
+        c == 'R' -> {
+          if (buffer.isNotEmpty()) {
+            result += Instruction.Move(buffer.toInt())
+            buffer = ""
+          }
+          result += Instruction.TurnRight
+        }
+        c == 'L' -> {
+          if (buffer.isNotEmpty()) {
+            result += Instruction.Move(buffer.toInt())
+            buffer = ""
+          }
+          result += Instruction.TurnLeft
+        }
+        else -> {
+          error("Unhandled char=$c")
+        }
+      }
+    }
+
+    var facing = Right
+
+    var position = board.startingPosition()
+
+    result.forEach { instruction ->
+      when (instruction) {
+        is Instruction.Move -> {
+          repeat(instruction.steps) {
+            val previousPosition = position
+            while (true) {
+              position = Point(
+                x = (position.x + facing.x).let {
+                  when {
+                    it < 0 -> width - 1
+                    it > width - 1 -> 0
+                    else -> it
+                  }
+                },
+                y = (position.y + facing.y).let {
+                  when {
+                    it < 0 -> height - 1
+                    it > height - 1 -> 0
+                    else -> it
+                  }
+                },
+              )
+              when (board[position.y][position.x]) {
+                Tile.Wall -> {
+                  position = previousPosition
+                  break
+                }
+                Tile.Floor -> break
+                Tile.Void -> continue
+              }
+            }
+          }
+        }
+        Instruction.TurnLeft -> facing = facing.turnLeft()
+        Instruction.TurnRight -> facing = facing.turnRight()
+      }
+    }
+    return 1000 * (position.y + 1) + 4 * (position.x + 1) + facing.score
+  }
+
+  private fun Board.startingPosition(): Point {
+    forEachIndexed { y, tiles ->
+      tiles.forEachIndexed { x, tile ->
+        if (tile == Tile.Floor) {
+          return Point(x, y)
+        }
+      }
+    }
+    error("No starting position found.")
+  }
+
+  enum class Facing(val x: Int, val y: Int, val score: Int) {
+    Left(-1, 0, 2),
+    Up(0, -1, 3),
+    Right(1, 0, 0),
+    Down(0, 1, 1);
+
+    fun turnLeft(): Facing = values().getOrNull(ordinal - 1) ?: values().last()
+    fun turnRight(): Facing = values().getOrNull(ordinal + 1) ?: values().first()
+  }
+
+  sealed interface Instruction {
+    @JvmInline
+    value class Move(val steps: Int) : Instruction
+    object TurnLeft : Instruction {
+      override fun toString() = "TurnLeft"
+    }
+
+    object TurnRight : Instruction {
+      override fun toString() = "TurnRight"
+    }
+  }
+
+  enum class Tile {
+    Wall, Floor, Void
+  }
+
+  override fun solvePart2(input: String) = TODO("Not yet implemented")
+}
---
+++ src/test/kotlin/de/woitaschek/aoc/year2022/Day22Test.kt
@@ -0,0 +1,15 @@
+package de.woitaschek.aoc.year2022
+
+import de.woitaschek.aoc.test
+import org.junit.jupiter.api.Test
+
+class Day22Test {
+
+  @Test
+  fun test() {
+    Day22.test(
+      part1 = 89224,
+      part1Test = 6032,
+    )
+  }
+}
