3663e18cdb5428b2db32095489d2ac82dd6fa332
Paul Woitaschek <woitaschek@gmail.com>
2022-12-30 23:44:47 +0100

    Solve 2020 day 7.

---
+++ src/test/kotlin/de/woitaschek/aoc/year2020/Day7Test.kt
@@ -0,0 +1,23 @@
+package de.woitaschek.aoc.year2020
+
+import de.woitaschek.aoc.test
+import org.junit.jupiter.api.Test
+
+class Day7Test {
+
+  @Test
+  fun part1() {
+    Day7.test(
+      part1Test = 4,
+      part1 = 370,
+    )
+  }
+
+  @Test
+  fun part2() {
+    Day7.test(
+      part2Test = 32,
+      part2 = 29547,
+    )
+  }
+}
