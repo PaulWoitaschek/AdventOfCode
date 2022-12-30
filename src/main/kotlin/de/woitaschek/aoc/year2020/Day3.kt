6611add85d16653bc89ff77d5618e80e97a62e3e
Paul Woitaschek <woitaschek@gmail.com>
2022-12-30 16:07:53 +0100

    Solve 2020 day 3.

---
+++ src/main/kotlin/de/woitaschek/aoc/year2020/Day3.kt
@@ -0,0 +1,38 @@
+package de.woitaschek.aoc.year2020
+
+import de.woitaschek.aoc.utils.Point
+import de.woitaschek.aoc.utils.Puzzle
+
+object Day3 : Puzzle(2020, 3) {
+
+  override fun solvePart1(input: String): Int = solve(input, Point(3, 1))
+
+  override fun solvePart2(input: String): Int = solve(
+    input,
+    Point(1, 1),
+    Point(3, 1),
+    Point(5, 1),
+    Point(7, 1),
+    Point(1, 2),
+  )
+
+  private fun solve(input: String, vararg slopes: Point): Int {
+    val treeMap = parseInput(input)
+    return slopes.map { countTreesOnSlope(treeMap, it) }.reduce(Int::times)
+  }
+
+  private fun countTreesOnSlope(
+    treeMap: List<List<Boolean>>,
+    slope: Point,
+  ): Int = generateSequence(Point(0, 0)) { it + slope }
+    .takeWhile { it.y < treeMap.size }
+    .count { (x, y) ->
+      val treeLine = treeMap[y]
+      treeLine[x.mod(treeLine.size)]
+    }
+
+  private fun parseInput(input: String): List<List<Boolean>> = input.lines().filter(String::isNotEmpty)
+    .map { treeLine ->
+      treeLine.map { it == '#' }
+    }
+}
---
+++ src/test/kotlin/de/woitaschek/aoc/year2020/Day3Test.kt
@@ -0,0 +1,23 @@
+package de.woitaschek.aoc.year2020
+
+import de.woitaschek.aoc.test
+import org.junit.jupiter.api.Test
+
+class Day3Test {
+
+  @Test
+  fun part1() {
+    Day3.test(
+      part1Test = 7,
+      part1 = 193,
+    )
+  }
+
+  @Test
+  fun part2() {
+    Day3.test(
+      part2Test = 336,
+      part2 = 1355323200
+    )
+  }
+}
