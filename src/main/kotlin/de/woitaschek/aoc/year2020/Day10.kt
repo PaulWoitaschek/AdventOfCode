84c7b752f99916851b1e639cb8229ad80e108f38
Paul Woitaschek <woitaschek@gmail.com>
2023-01-01 22:55:50 +0100

    Solve year 2020 day 10.

---
+++ src/main/kotlin/de/woitaschek/aoc/year2020/Day10.kt
@@ -0,0 +1,34 @@
+package de.woitaschek.aoc.year2020
+
+import de.woitaschek.aoc.utils.Puzzle
+
+object Day10 : Puzzle(2020, 10) {
+
+  override fun solvePart1(input: String): Int {
+    val differences = powerConnections(input).zipWithNext { first, second ->
+      second - first
+    }
+    return differences.count { it == 1 } * differences.count { it == 3 }
+  }
+
+  override fun solvePart2(input: String): Long {
+    val paths = mutableMapOf(0 to 1L)
+    val connections = powerConnections(input)
+    connections.drop(1).forEach { connection ->
+      paths[connection] = (1..3).sumOf { step ->
+        paths.getOrDefault(connection - step, 0)
+      }
+    }
+    return paths.getValue(connections.last())
+  }
+
+  private fun powerConnections(input: String): List<Int> = input.lines().filter { it.isNotEmpty() }
+    .map { it.toInt() }
+    .sorted()
+    .let {
+      it.toMutableList().apply {
+        add(0, 0)
+        add(last() + 3)
+      }
+    }
+}
