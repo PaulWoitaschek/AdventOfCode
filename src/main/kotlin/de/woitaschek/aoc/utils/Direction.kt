c8b0c72c8f884ed8cbb9522e990194ac4c9b7143
Paul Woitaschek <woitaschek@gmail.com>
2023-01-15 22:29:31 +0100

    Solve year 2020 day 12.

--- src/main/kotlin/de/woitaschek/aoc/utils/Direction.kt
+++ src/main/kotlin/de/woitaschek/aoc/utils/Direction.kt
@@ -1,7 +1,15 @@
 package de.woitaschek.aoc.utils
 
 enum class Direction(var char: Char) {
-  Left('L'), Right('R'), Up('U'), Down('D');
+  Left('L'), Up('U'), Right('R'), Down('D');
+
+  fun clockwise(): Direction {
+    return values().getOrNull(ordinal + 1) ?: values().first()
+  }
+
+  fun counterClockwise(): Direction {
+    return values().getOrNull(ordinal - 1) ?: values().last()
+  }
 
   companion object {
     fun parse(input: Char) = values().first { it.char == input }
