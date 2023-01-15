4fc995b6e808e467569c943cb03c8aff2782938f
Paul Woitaschek <woitaschek@gmail.com>
2023-01-15 09:56:24 +0100

    Solve year 2020 day 16.

---
+++ src/test/kotlin/de/woitaschek/aoc/year2020/Day16Test.kt
@@ -0,0 +1,22 @@
+package de.woitaschek.aoc.year2020
+
+import de.woitaschek.aoc.test
+import org.junit.jupiter.api.Test
+
+class Day16Test {
+
+  @Test
+  fun part1() {
+    Day16.test(
+      part1Test = 71,
+      part1 = 29851,
+    )
+  }
+
+  @Test
+  fun part2() {
+    Day16.test(
+      part2 = 3029180675981,
+    )
+  }
+}
