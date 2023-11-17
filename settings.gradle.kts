a2e5f7aec1e12057e0ed2ab5215a7a6bf6fa5942
Paul Woitaschek <woitaschek@gmail.com>
2023-11-17 09:37:30 +0100

    Remove compose and the benchmark

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,13 +3,11 @@ rootProject.name = "AdventOfCode"
 pluginManagement {
   repositories {
     gradlePluginPortal()
-    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
   }
 }
 
 dependencyResolutionManagement {
   repositories {
     mavenCentral()
-    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
   }
 }
