906afaf6a680b85496df40d1a76007e85a3aec7d
Paul Woitaschek <woitaschek@gmail.com>
2023-11-18 16:51:08 +0100

    Solve 2019, day 7

--- src/main/kotlin/de/woitaschek/aoc/year2019/Day5.kt
+++ src/main/kotlin/de/woitaschek/aoc/year2019/Day5.kt
@@ -2,7 +2,7 @@ package de.woitaschek.aoc.year2019
 
 import de.woitaschek.aoc.utils.Puzzle
 import de.woitaschek.aoc.utils.toCommaSeparatedIntList
-import kotlin.math.pow
+import kotlinx.coroutines.runBlocking
 
 object Day5 : Puzzle<Int, Int>(2019, 5) {
 
@@ -14,89 +14,10 @@ object Day5 : Puzzle<Int, Int>(2019, 5) {
     values: List<Int>,
     inputValue: Int,
   ): Int {
-    return Computer(values.toMutableList(), inputValue).run()
-  }
-
-  private class Computer(
-    private val instructions: MutableList<Int>,
-    private val input: Int,
-  ) {
-
-    private var pointer = 0
-    private val output = StringBuilder()
-    private var finished = false
-
-    fun run(): Int {
-      while (!finished) {
-        runInstruction()
-      }
-      return output.toString().toInt()
+    val computer = IntCodeComputer(values.toMutableList(), mutableListOf(inputValue))
+    runBlocking {
+      computer.run()
     }
-
-    private fun read(index: Int): Int {
-      val value = instructions[pointer + index + 1]
-      val instructionValue = instructions[pointer]
-      return when (val v = instructionValue / 10.toFloat().pow(index + 2).toInt() % 10) {
-        0 -> instructions[value]
-        1 -> value
-        else -> error("Invalid mode=$v")
-      }
-    }
-
-    private fun write(
-      index: Int,
-      value: Int,
-    ) {
-      instructions[instructions[pointer + index + 1]] = value
-    }
-
-    private fun runInstruction() {
-      when (val opCode = instructions[pointer] % 100) {
-        1 -> {
-          write(2, read(0) + read(1))
-          pointer += 4
-        }
-        2 -> {
-          write(2, read(0) * read(1))
-          pointer += 4
-        }
-        3 -> {
-          write(0, input)
-          pointer += 2
-        }
-        4 -> {
-          output.append(read(0))
-          pointer += 2
-        }
-        5 -> {
-          if (read(0) != 0) {
-            pointer = read(1)
-          } else {
-            pointer += 3
-          }
-        }
-        6 -> {
-          if (read(0) == 0) {
-            pointer = read(1)
-          } else {
-            pointer += 3
-          }
-        }
-        7 -> {
-          val value = if (read(0) < read(1)) 1 else 0
-          write(2, value)
-          pointer += 4
-        }
-        8 -> {
-          val value = if (read(0) == read(1)) 1 else 0
-          write(2, value)
-          pointer += 4
-        }
-        99 -> {
-          finished = true
-        }
-        else -> error("Invalid opCode=$opCode")
-      }
-    }
+    return computer.fullOutput
   }
 }
