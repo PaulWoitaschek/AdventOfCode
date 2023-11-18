ed6b6d0119ed964fe6fd21c939ae1e23dfaa7390
Paul Woitaschek <woitaschek@gmail.com>
2023-11-18 06:52:39 +0100

    Solve 2019, day 4

--- src/main/kotlin/de/woitaschek/aoc/utils/Parsing.kt
+++ src/main/kotlin/de/woitaschek/aoc/utils/Parsing.kt
@@ -3,3 +3,4 @@ package de.woitaschek.aoc.utils
 fun String.toLineSeparatedIntList(): List<Int> = lines().mapNotNull(String::toIntOrNull)
 fun String.toCommaSeparatedIntList(): List<Int> = lines().first().split(",").map { it.toInt() }
 fun String.toLineSeparatedStringList(): List<String> = lines().filter(String::isNotEmpty)
+fun String.toSingleLine(): String = lines().first()
