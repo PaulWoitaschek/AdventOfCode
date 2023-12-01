7e31769759de43cfa161d91c70aaf4d9c6ab11ee
Paul Woitaschek <woitaschek@gmail.com>
2023-12-01 09:30:07 +0100

    Make use of findAnyOf and findLastAnyOf

--- src/main/kotlin/aoc/year2023/Day1.kt
+++ src/main/kotlin/aoc/year2023/Day1.kt
@@ -16,7 +16,7 @@ object Day1 : Puzzle<Int, Int>(2023, 1) {
   }
 
   override fun solvePart2(input: String): Int {
-    val numberNames = (1..9).associateWith { number ->
+    val numbers = (1..9).associateBy { it.toString() } + (1..9).associateBy { number ->
       when (number) {
         1 -> "one"
         2 -> "two"
@@ -27,29 +27,15 @@ object Day1 : Puzzle<Int, Int>(2023, 1) {
         7 -> "seven"
         8 -> "eight"
         9 -> "nine"
-        else -> error("Invalid number")
+        else -> error("Invalid number $number")
       }
-    }.mapValues { listOf(it.value, it.key.toString()) }
-
-    fun findNumbers(string: String): Map<Int, Int> {
-      val foundNumbers = mutableMapOf<Int, Int>()
-      numberNames.forEach { (number, numberValues) ->
-        numberValues.forEach { numberValue ->
-          val index = string.indexOf(numberValue)
-          if (index != -1) foundNumbers[index] = number
-          val lastIndex = string.lastIndexOf(numberValue)
-          if (lastIndex != -1) foundNumbers[lastIndex] = number
-        }
-      }
-      return foundNumbers
     }
 
     return input.toLineSeparatedStringList()
       .map { line ->
-        val foundNumbers = findNumbers(line)
-        val firstDigit = foundNumbers.minBy { it.key }.value
-        val lastDigit = foundNumbers.maxBy { it.key }.value
-        "$firstDigit$lastDigit".toInt()
+        val min = numbers.getValue(line.findAnyOf(numbers.keys)!!.second)
+        val max = numbers.getValue(line.findLastAnyOf(numbers.keys)!!.second)
+        "$min$max".toInt()
       }
       .fold(0, Int::plus)
   }
