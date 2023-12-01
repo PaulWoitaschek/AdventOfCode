e3e8c0a5711be1aaace44d77a32f7558330d6ffa
Paul Woitaschek <woitaschek@gmail.com>
2023-12-01 06:36:55 +0100

    Solve 2023, day 1

---
+++ src/test/kotlin/aoc/year2023/Day1Test.kt
@@ -0,0 +1,45 @@
+package aoc.year2023
+
+import aoc.utils.test
+import io.kotest.matchers.ints.shouldBeExactly
+import org.junit.jupiter.api.Test
+
+class Day1Test {
+
+  @Test
+  fun part1Test() {
+    Day1.solvePart1(
+      """
+      1abc2
+      pqr3stu8vwx
+      a1b2c3d4e5f
+      treb7uchet
+      """.trimIndent(),
+    ) shouldBeExactly 142
+  }
+
+  @Test
+  fun part1() {
+    Day1.test(part1 = 56042)
+  }
+
+  @Test
+  fun part2Test() {
+    Day1.solvePart2(
+      """
+      two1nine
+      eightwothree
+      abcone2threexyz
+      xtwone3four
+      4nineeightseven2
+      zoneight234
+      7pqrstsixteen
+      """.trimIndent(),
+    ) shouldBeExactly 281
+  }
+
+  @Test
+  fun part2() {
+    Day1.test(part2 = 55358)
+  }
+}
