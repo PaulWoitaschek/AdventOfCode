6789cdfaca70cdcf52dffe9faf98e3f32470d0ac
Paul Woitaschek <woitaschek@gmail.com>
2023-12-02 08:56:51 +0100

    Extract out a solve function

--- src/main/kotlin/aoc/year2023/Day2.kt
+++ src/main/kotlin/aoc/year2023/Day2.kt
@@ -5,13 +5,9 @@ import aoc.utils.toLineSeparatedStringList
 
 object Day2 : Puzzle<Int, Int>(2023, 2) {
 
-  override fun solvePart1(input: String): Int {
-    val bagContents = mapOf(
-      "red" to 12,
-      "green" to 13,
-      "blue" to 14,
-    )
-    return parse(input)
+  override fun solvePart1(input: String): Int = solve(input) { games ->
+    val bagContents = mapOf("red" to 12, "green" to 13, "blue" to 14)
+    games
       .filter { game ->
         game.shownCubes.all { round ->
           round.all { (shownColor, shownAmount) ->
@@ -22,30 +18,38 @@ object Day2 : Puzzle<Int, Int>(2023, 2) {
       .sumOf { it.id }
   }
 
-  override fun solvePart2(input: String): Int = parse(input).sumOf { game ->
-    buildMap {
-      game.shownCubes.forEach { sets ->
-        sets.forEach { (color, amount) ->
-          put(color, maxOf(getOrDefault(color, amount), amount))
+  override fun solvePart2(input: String): Int = solve(input) { games ->
+    games.sumOf { game ->
+      buildMap {
+        game.shownCubes.forEach { sets ->
+          sets.forEach { (color, amount) ->
+            put(color, maxOf(getOrDefault(color, amount), amount))
+          }
         }
-      }
-    }.values.reduce(Int::times)
+      }.values.reduce(Int::times)
+    }
   }
 
-  private fun parse(input: String): List<Game> = input.toLineSeparatedStringList()
-    .map { line ->
-      val (gameIdValue, gameValue) = line.split(": ")
-      Game(
-        id = gameIdValue.removePrefix("Game ").toInt(),
-        shownCubes = gameValue.split("; ").map { set ->
-          set.split(", ")
-            .associate { amountWithColor ->
-              val (amount, color) = amountWithColor.split(" ")
-              color to amount.toInt()
-            }
-        },
-      )
-    }
+  private fun solve(
+    input: String,
+    solve: (List<Game>) -> Int,
+  ): Int {
+    val games = input.toLineSeparatedStringList()
+      .map { line ->
+        val (gameIdValue, gameValue) = line.split(": ")
+        Game(
+          id = gameIdValue.removePrefix("Game ").toInt(),
+          shownCubes = gameValue.split("; ").map { set ->
+            set.split(", ")
+              .associate { amountWithColor ->
+                val (amount, color) = amountWithColor.split(" ")
+                color to amount.toInt()
+              }
+          },
+        )
+      }
+    return solve(games)
+  }
 
   private data class Game(
     val id: Int,
