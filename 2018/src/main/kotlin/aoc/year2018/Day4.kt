a5f794fb0a65aae377e0c11ca74f8f71d1b1a7de
Paul Woitaschek <woitaschek@gmail.com>
2023-12-08 05:39:01 +0100

    Make the Guard immutable

--- 2018/src/main/kotlin/aoc/year2018/Day4.kt
+++ 2018/src/main/kotlin/aoc/year2018/Day4.kt
@@ -6,18 +6,18 @@ object Day4 : Puzzle<Int, Int>(year = 2018, day = 4) {
 
   override fun solvePart1(input: String): Int {
     val sleepyGuard = guards(input).maxBy { it.sleepTimes.size }
-    return sleepyGuard.id * sleepyGuard.mostSleepingMinute().minute
+    return sleepyGuard.id * sleepyGuard.mostSleepingMinute
   }
 
   override fun solvePart2(input: String): Int {
-    return guards(input).maxBy { it.mostSleepingMinute().times }
-      .let { it.id * it.mostSleepingMinute().minute }
+    return guards(input).maxBy { it.occurrencesOfMostSleepingMinute }
+      .let { it.id * it.mostSleepingMinute }
   }
 
   private fun guards(input: String): List<Guard> {
     val wholeRegex = ".*?:(\\d+)] (.*)$".toRegex()
     val guardRegex = """^Guard #(\d+)""".toRegex()
-    val guards = mutableMapOf<Int, Guard>()
+    val guards = mutableMapOf<Int, MutableList<Int>>()
     var currentGuard = 0
     var fellAsleepAt = 0
     input.lines().sorted().forEach { line ->
@@ -28,30 +28,32 @@ object Day4 : Puzzle<Int, Int>(year = 2018, day = 4) {
           fellAsleepAt = minute
         }
         "wakes up" -> {
-          guards.getOrPut(currentGuard) { Guard(currentGuard, mutableListOf()) }
-            .sleepTimes.addAll(fellAsleepAt..<minute)
+          guards.getOrPut(currentGuard) { mutableListOf() }
+            .addAll(fellAsleepAt..<minute)
         }
         else -> {
           currentGuard = guardRegex.find(rest)!!.groupValues[1].toInt()
         }
       }
     }
-    return guards.values.toList()
+    return guards.map { Guard(it.key, it.value) }
   }
 }
 
-private data class Guard(val id: Int, val sleepTimes: MutableList<Int>) {
-  fun mostSleepingMinute(): SleepingMinute {
+private data class Guard(val id: Int, val sleepTimes: List<Int>) {
+
+  val mostSleepingMinute: Int
+  val occurrencesOfMostSleepingMinute: Int
+
+  init {
     if (sleepTimes.isEmpty()) {
-      return SleepingMinute(0, 0)
+      mostSleepingMinute = 0
+      occurrencesOfMostSleepingMinute = 0
+    } else {
+      val entry = sleepTimes.groupingBy { it }.eachCount()
+        .maxBy { it.value }
+      mostSleepingMinute = entry.key
+      occurrencesOfMostSleepingMinute = entry.value
     }
-    return sleepTimes.groupingBy { it }.eachCount()
-      .maxBy { it.value }
-      .let { SleepingMinute(minute = it.key, times = it.value) }
   }
 }
-
-private data class SleepingMinute(
-  val minute: Int,
-  val times: Int,
-)
