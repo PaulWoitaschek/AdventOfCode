753fc0e43054aff18e48d7d68be5548a35496e45
Paul Woitaschek <woitaschek@gmail.com>
2023-12-09 08:14:31 +0100

    Optimize the solution further by reversing the input

--- 2023/src/main/kotlin/aoc/year2023/Day9.kt
+++ 2023/src/main/kotlin/aoc/year2023/Day9.kt
@@ -10,25 +10,14 @@ object Day9 : Puzzle<Long, Long>(year = 2023, day = 9) {
   private fun solve(
     input: String,
     addRight: Boolean,
-  ): Long {
-    return input.lines()
-      .map { it.split(" ").map(String::toLong) }
-      .sumOf { predict(history = it, addRight = addRight) }
+  ): Long = input.lines().sumOf { line ->
+    val history = line.split(" ").map(String::toLong)
+    predict(if (addRight) history else history.reversed())
   }
 
-  private fun predict(
-    history: List<Long>,
-    addRight: Boolean,
-  ): Long {
-    return if (history.all { it == 0L }) {
-      return 0L
-    } else {
-      val differences = history.windowed(2) { it[1] - it[0] }
-      if (addRight) {
-        history.last() + predict(history = differences, addRight = true)
-      } else {
-        history.first() - predict(history = differences, addRight = false)
-      }
-    }
+  private fun predict(history: List<Long>): Long = if (history.all { it == 0L }) {
+    0L
+  } else {
+    history.last() + predict(history = history.windowed(2) { it[1] - it[0] })
   }
 }
