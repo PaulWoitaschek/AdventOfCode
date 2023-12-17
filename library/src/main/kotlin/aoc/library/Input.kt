b0430414b94d169951c3d32f474f400a63b6069d
Paul Woitaschek <woitaschek@gmail.com>
2023-12-17 21:32:56 +0100

    Remove the test input util

--- library/src/main/kotlin/aoc/library/Input.kt
+++ library/src/main/kotlin/aoc/library/Input.kt
@@ -9,11 +9,7 @@ import java.io.File
 
 fun <Part1, Part2> Puzzle<Part1, Part2>.solvePart1(): Part1 = solvePart1(input())
 
-fun <Part1, Part2> Puzzle<Part1, Part2>.solvePart1WithTestInput(): Part1 = solvePart1(testInput())
-
 fun <Part1, Part2> Puzzle<Part1, Part2>.solvePart2(): Part2 = solvePart2(input())
-
-fun <Part1, Part2> Puzzle<Part1, Part2>.solvePart2WithTestInput(): Part2 = solvePart2(testInput())
 
 fun Puzzle<*, *>.input(): String = aocInput(day = day)
 
@@ -49,12 +45,5 @@ private fun aocInput(day: Int): String {
       }
     }
   }
-  return file.readText().removeSuffix("\n")
-}
-
-private fun Puzzle<*, *>.testInput(): String = aocTestInput(day = day)
-
-private fun aocTestInput(day: Int): String {
-  val file = File("src/main/resources/$day-test.txt")
   return file.readText().removeSuffix("\n")
 }
