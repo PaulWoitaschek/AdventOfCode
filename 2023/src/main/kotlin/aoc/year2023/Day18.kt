74f05c2a1888e22260a29778b3418b6e62fc67e5
Paul Woitaschek <woitaschek@gmail.com>
2023-12-18 12:06:57 +0100

    Simplify the part 1 implementation by merely counting the steps

--- 2023/src/main/kotlin/aoc/year2023/Day18.kt
+++ 2023/src/main/kotlin/aoc/year2023/Day18.kt
@@ -4,30 +4,22 @@ import aoc.library.Direction
 import aoc.library.Point
 import aoc.library.Puzzle
 import aoc.library.move
-import kotlin.math.ceil
 
 object Day18 : Puzzle<Int, Int>(day = 18) {
 
   override fun solvePart1(input: String): Int {
     val instructions = input.lines().map(Instruction::parse)
-
     val points = instructions.runningFold(Point.Zero) { point, instruction ->
       point.move(instruction.direction, instruction.steps)
     }
-
-    val border = mutableListOf(Point.Zero)
-    instructions.forEach { instruction ->
-      repeat(instruction.steps) {
-        border.add(border.last().move(instruction.direction))
-      }
-    }
-    return ceil((polygonSize(points) * 2 + border.size) / 2f).toInt()
+    val borderSize = instructions.sumOf { it.steps }
+    return polygonSize(points) + borderSize / 2 + 1
   }
 
   private fun polygonSize(points: List<Point>): Int {
-    val a = points.map { it.x }.zip(points.drop(1).map { it.y }) { x, y -> x * y }
-    val b = points.map { it.x }.drop(1).zip(points.map { it.y }) { x, y -> x * y }
-    return a.zip(b) { l, r -> l - r }.sum() / 2
+    val positive = points.zip(points.drop(1)) { left, right -> left.x * right.y }
+    val negative = points.drop(1).zip(points) { left, right -> left.x * right.y }
+    return positive.zip(negative) { l, r -> l - r }.sum() / 2
   }
 
   override fun solvePart2(input: String): Int {
