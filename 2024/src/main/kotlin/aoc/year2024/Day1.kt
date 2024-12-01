16bdd3c4082a3c924a7a0bd0a5c3d08f06a73825
Paul Woitaschek <woitaschek@gmail.com>
2024-12-01 13:56:54 +0100

    Solve 2024, day 1

--- 2024/src/main/kotlin/aoc/year2024/Day1.kt
+++ 2024/src/main/kotlin/aoc/year2024/Day1.kt
@@ -1,13 +1,32 @@
 package aoc.year2024
 
 import aoc.library.Puzzle
+import kotlin.math.abs
 
-public object Day1 : Puzzle<Int, Int>(day = 1) {
-  override fun solvePart1(input: String): Int {
-    TODO()
+object Day1 : Puzzle<Long, Long>(day = 1) {
+
+  override fun solvePart1(input: String): Long {
+    val (left, right) = parse(input)
+    return left.sorted().zip(right.sorted()) { l, r ->
+      abs(l - r)
+    }.sum()
   }
 
-  override fun solvePart2(input: String): Int {
-    TODO()
+  override fun solvePart2(input: String): Long {
+    val (left, right) = parse(input)
+    val grouping = right.groupingBy { it }.eachCount()
+    return left.sumOf { it * grouping.getOrElse(it) { 0 } }
+  }
+
+  private fun parse(input: String): Pair<List<Long>, List<Long>> {
+    val left = mutableListOf<Long>()
+    val right = mutableListOf<Long>()
+    val numberRegex = "(\\d+) *(\\d+)".toRegex()
+    input.lines().forEach { line ->
+      val (leftValue, rightValue) = numberRegex.find(line)!!.destructured.toList().map(String::toLong)
+      left += leftValue
+      right += rightValue
+    }
+    return left to right
   }
 }
--- 2024/src/test/kotlin/aoc/year2024/Day1Test.kt
+++ 2024/src/test/kotlin/aoc/year2024/Day1Test.kt
@@ -2,33 +2,46 @@ package aoc.year2024
 
 import aoc.library.solvePart1
 import aoc.library.solvePart2
-import io.kotest.matchers.ints.shouldBeExactly
-import org.junit.jupiter.api.Disabled
+import io.kotest.matchers.longs.shouldBeExactly
 import org.junit.jupiter.api.Test
 
 class Day1Test {
 
   @Test
-  @Disabled
   fun part1TestInput() {
-    Day1.solvePart1() shouldBeExactly 42
+    Day1.solvePart1(
+      """
+      3   4
+      4   3
+      2   5
+      1   3
+      3   9
+      3   3
+      """.trimIndent(),
+    ) shouldBeExactly 11
   }
 
   @Test
-  @Disabled
   fun part1() {
-    Day1.solvePart1() shouldBeExactly 42
+    Day1.solvePart1() shouldBeExactly 2176849
   }
 
   @Test
-  @Disabled
   fun part2TestInput() {
-    Day1.solvePart2() shouldBeExactly 42
+    Day1.solvePart2(
+      """
+      3   4
+      4   3
+      2   5
+      1   3
+      3   9
+      3   3
+      """.trimIndent(),
+    ) shouldBeExactly 31
   }
 
   @Test
-  @Disabled
   fun part2() {
-    Day1.solvePart2() shouldBeExactly 42
+    Day1.solvePart2() shouldBeExactly 23384288
   }
 }
