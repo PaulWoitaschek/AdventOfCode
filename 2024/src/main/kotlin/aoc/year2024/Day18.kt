60e30253562e6b87e08c5fe1f6a9e65191ccdced
Paul Woitaschek <woitaschek@gmail.com>
2024-12-19 21:08:51 +0100

    Clean up day 18

--- 2024/src/main/kotlin/aoc/year2024/Day18.kt
+++ 2024/src/main/kotlin/aoc/year2024/Day18.kt
@@ -6,108 +6,62 @@ import aoc.library.Rect
 import java.util.PriorityQueue
 
 object Day18 : Puzzle<Int, Point>(day = 18) {
+
   override fun solvePart1(input: String): Int = solvePart1(input = input, bytes = 1024, size = 70)
+
+  override fun solvePart2(input: String): Point = solvePart2(input, size = 70)
 
   fun solvePart1(
     input: String,
     bytes: Int,
     size: Int,
   ): Int {
-    val corrupted = input.lines()
-      .map {
-        val (x, y) = it.split(",")
-        Point(x.toInt(), y.toInt())
-      }
-      .take(bytes)
-
-    val end = Point(size, size)
-
-    data class State(val point: Point, val steps: Int)
-
-    val queue = PriorityQueue(compareBy<State> { it.steps })
-    queue.add(State(Point(0, 0), 0))
-
-    val bounds = Rect(0, 0, size, size)
-
-    val visited = mutableSetOf<Point>()
-    while (true) {
-      val state = queue.remove()!!
-      if (!visited.add(state.point)) continue
-      if (state.point == end) {
-        return state.steps
-      }
-      queue += state.point.adjacentOrthogonal()
-        .filter { it in bounds && it !in corrupted }
-        .map { State(it, state.steps + 1) }
-    }
+    val corrupted = parse(input).take(bytes)
+    return solveBFS(corrupted, size)!!
   }
 
   fun solvePart2(
     input: String,
     size: Int,
   ): Point {
-    val allBytes = input.lines()
-      .map {
-        val (x, y) = it.split(",")
-        Point(x.toInt(), y.toInt())
-      }
+    val allBytes = parse(input)
     val byte = (0..allBytes.lastIndex).toList()
-      .binarySearchHighestMatching { isSolvable(allBytes, it, size) }!!
+      .binarySearchHighestMatching { solveBFS(allBytes.take(it), size) != null }!!
     return allBytes.elementAt(byte)
   }
 
-  private fun isSolvable(
-    allBytes: List<Point>,
-    bytes: Int,
-    size: Int,
-  ): Boolean {
-    val corrupted = allBytes
-      .take(bytes)
+  private fun parse(input: String): List<Point> = input.lines()
+    .map {
+      val (x, y) = it.split(",")
+      Point(x.toInt(), y.toInt())
+    }
 
-    val end = Point(size, size)
+  private fun solveBFS(
+    corrupted: List<Point>,
+    size: Int,
+  ): Int? {
+    val bounds = Rect(0, 0, size, size)
+    val target = Point(size, size)
 
     data class State(val point: Point, val steps: Int)
 
     val queue = PriorityQueue(compareBy<State> { it.steps })
     queue.add(State(Point(0, 0), 0))
 
-    val bounds = Rect(0, 0, size, size)
-
     val visited = mutableSetOf<Point>()
-    while (true) {
-      if (queue.isEmpty()) {
-        return false
-      }
+    while (queue.isNotEmpty()) {
       val state = queue.remove()
+
       if (!visited.add(state.point)) continue
-      if (state.point == end) {
-        return true
-      }
+
+      if (state.point == target) return state.steps
+
       queue += state.point.adjacentOrthogonal()
         .filter { it in bounds && it !in corrupted }
         .map { State(it, state.steps + 1) }
     }
+    return null
   }
-
-  override fun solvePart2(input: String): Point = solvePart2(input, 70)
-}
-
-fun <T> List<T>.binarySearchLowestMatching(condition: (T) -> Boolean): Int? {
-  var low = 0
-  var high = size - 1
-  var result: Int? = null
-
-  while (low <= high) {
-    val mid = low + (high - low) / 2
-    if (condition(this[mid])) {
-      result = mid
-      high = mid - 1
-    } else {
-      low = mid + 1
-    }
-  }
-
-  return result
 }
 
 fun <T> List<T>.binarySearchHighestMatching(condition: (T) -> Boolean): Int? {
