db96174e8ad946fcdfc8cc7ae061ab427c13aace
Paul Woitaschek <woitaschek@gmail.com>
2024-12-23 18:02:10 +0100

    Precompute the connections

--- 2024/src/main/kotlin/aoc/year2024/Day23.kt
+++ 2024/src/main/kotlin/aoc/year2024/Day23.kt
@@ -6,7 +6,7 @@ object Day23 : Puzzle<Int, List<String>>(day = 23) {
 
   override fun solvePart1(input: String): Int {
     val lanPartyFinder = LanPartyFinder.parse(input)
-    val connectionsWithT = lanPartyFinder.connections.filter { connection ->
+    val connectionsWithT = lanPartyFinder.links.filter { connection ->
       connection.any { it.startsWith("t") }
     }.toSet()
     return lanPartyFinder.addMoreConnections(connectionsWithT).size
@@ -15,7 +15,7 @@ object Day23 : Puzzle<Int, List<String>>(day = 23) {
   override fun solvePart2(input: String): List<String> {
     val lanPartyFinder = LanPartyFinder.parse(input)
 
-    var groups = lanPartyFinder.connections
+    var groups = lanPartyFinder.links
     while (true) {
       val newGroups = lanPartyFinder.addMoreConnections(groups)
       if (newGroups.size == 1) {
@@ -26,19 +26,18 @@ object Day23 : Puzzle<Int, List<String>>(day = 23) {
     }
   }
 
-  private class LanPartyFinder(val connections: Set<Set<String>>) {
+  private class LanPartyFinder(val links: Set<Set<String>>) {
 
-    private val connectionsCache = mutableMapOf<String, Set<String>>()
-    fun connections(from: String): Set<String> = connectionsCache.getOrPut(from) {
-      connections.filter { from in it }
-        .map { elements -> elements.first { it != from } }
-        .toSet()
-    }
+    private val connections: Map<String, Set<String>> = links.flatten().toSet()
+      .associateWith { from ->
+        links.filter { from in it }
+          .map { elements -> elements.first { it != from } }
+          .toSet()
+      }
 
     fun addMoreConnections(found: Set<Set<String>>): Set<Set<String>> = found.flatMap { current ->
-      current.asSequence().flatMap { connections(it) }.distinct()
-        .filter { it !in current }
-        .filter { connections(it).containsAll(current) }
+      current.flatMap { connections.getValue(it) }.toSet()
+        .filter { it !in current && connections.getValue(it).containsAll(current) }
         .map { candidate ->
           current + candidate
         }
