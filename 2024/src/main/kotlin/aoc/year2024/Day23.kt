f158b8aeef05c697d0706c0912a8d8c1be80b82f
Paul Woitaschek <woitaschek@gmail.com>
2024-12-23 17:44:28 +0100

    Clean-up and deduplicate.

--- 2024/src/main/kotlin/aoc/year2024/Day23.kt
+++ 2024/src/main/kotlin/aoc/year2024/Day23.kt
@@ -5,51 +5,19 @@ import aoc.library.Puzzle
 object Day23 : Puzzle<Int, List<String>>(day = 23) {
 
   override fun solvePart1(input: String): Int {
-    val connections = input.lines().map {
-      it.split("-").toSet()
-    }
-    val candidates = connections.flatten().toSet().filter { it.startsWith("t") }
-
-    val interConnectedTs = mutableSetOf<Set<String>>()
-
-    fun connections(from: String): Set<String> = connections.filter { from in it }
-      .map { it.single { it != from } }
-      .toSet()
-
-    candidates.forEach { a ->
-      connections(a).forEach { b ->
-        connections(b).forEach { c ->
-          if (a in connections(c)) {
-            interConnectedTs.add(setOf(a, b, c))
-          }
-        }
-      }
-    }
-    //  println(interConnectedTs.filter { it.size == 3 }.joinToString("\n") { it.joinToString(",") })
-
-    /**
-     * ka-co
-     * ta-co
-     * de-co
-     * ta-ka
-     * de-ta
-     * ka-de
-     */
-
-    return interConnectedTs.size // .count { it.size == 3 }
+    val lanPartyFinder = LanPartyFinder.parse(input)
+    val connectionsWithT = lanPartyFinder.connections.filter { connection ->
+      connection.any { it.startsWith("t") }
+    }.toSet()
+    return lanPartyFinder.addMoreConnections(connectionsWithT).size
   }
 
   override fun solvePart2(input: String): List<String> {
-    val connections = input.lines().map {
-      it.split("-").toSet()
-    }.toSet()
+    val lanPartyFinder = LanPartyFinder.parse(input)
 
-    var groups = connections
-    var i = 1
+    var groups = lanPartyFinder.connections
     while (true) {
-      println("iteration ${i++}")
-      val newGroups = findConnectionGroups(connections, groups)
-      println("have ${newGroups.size} groups")
+      val newGroups = lanPartyFinder.addMoreConnections(groups)
       if (newGroups.size == 1) {
         return newGroups.single().sorted()
       } else {
@@ -58,29 +26,38 @@ object Day23 : Puzzle<Int, List<String>>(day = 23) {
     }
   }
 
-  fun findConnectionGroups(
-    connections: Set<Set<String>>,
-    found: Set<Set<String>>,
-  ): Set<Set<String>> {
-    val connectionsCache = mutableMapOf<String, Set<String>>()
+  private class LanPartyFinder(val connections: Set<Set<String>>) {
+
+    private val allElements = connections.flatten().toSet()
+    private val connectionsCache = mutableMapOf<String, Set<String>>()
     fun connections(from: String): Set<String> = connectionsCache.getOrPut(from) {
       connections.filter { from in it }
-        .map { it.single { it != from } }
+        .map { elements -> elements.first { it != from } }
         .toSet()
     }
 
-    val allElements = connections.flatten().toSet()
-    val new = mutableSetOf<Set<String>>()
-    found.forEach { current ->
-      allElements.forEach { candidate ->
-        if (candidate !in current) {
-          val shouldAdd = connections(candidate).containsAll(current)
-          if (shouldAdd) {
-            new += current + candidate
+    fun addMoreConnections(found: Set<Set<String>>): Set<Set<String>> {
+      val new = mutableSetOf<Set<String>>()
+      found.forEach { current ->
+        allElements.forEach { candidate ->
+          if (candidate !in current) {
+            val shouldAdd = connections(candidate).containsAll(current)
+            if (shouldAdd) {
+              new += current + candidate
+            }
           }
         }
       }
+      return new
     }
-    return new
+
+    companion object {
+      fun parse(input: String): LanPartyFinder {
+        val connections = input.lines().map {
+          it.split("-").toSet()
+        }.toSet()
+        return LanPartyFinder(connections)
+      }
+    }
   }
 }
