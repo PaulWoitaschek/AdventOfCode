86ee52978443f641cd23b7a4680800a386adb217
Paul Woitaschek <woitaschek@gmail.com>
2025-12-02 08:34:04 +0100

    Simplify the parse loop

--- 2025/src/main/kotlin/aoc/year2025/Day2.kt
+++ 2025/src/main/kotlin/aoc/year2025/Day2.kt
@@ -9,17 +9,16 @@ object Day2 : Puzzle<Long, Long>(day = 2) {
   override fun solvePart1(input: String): Long = solve(input, ::isValidId1)
 
   fun isValidId1(id: Long): Boolean {
-    val str = id.toString()
-    return str.take(str.length / 2) != str.drop(str.length / 2)
+    val idString = id.toString()
+    return idString.take(idString.length / 2) != idString.drop(idString.length / 2)
   }
 
   override fun solvePart2(input: String): Long = solve(input, ::isValidId2)
 
   fun isValidId2(id: Long): Boolean {
-    val length = id.toString().length
-    return (1..length / 2).none { chunkWidth ->
-      val chunks = id.toString().chunked(chunkWidth)
-      chunks.size > 1 && chunks.toSet().size == 1
+    val idString = id.toString()
+    return (1..idString.length / 2).none { chunkWidth ->
+      idString.chunked(chunkWidth).toSet().size == 1
     }
   }
 
@@ -28,12 +27,10 @@ object Day2 : Puzzle<Long, Long>(day = 2) {
     isValid: (Long) -> Boolean,
   ): Long = input.replace("\n", "")
     .split(",")
-    .map {
-      val (from, to) = it.split("-")
-      from.toLong() to to.toLong()
-    }.flatMap {
-      (it.first..it.second).filter { number ->
-        !isValid(number)
-      }
-    }.sum()
+    .flatMap {
+      val (from, to) = it.split("-").map(String::toLong)
+      from..to
+    }
+    .filterNot { isValid(it) }
+    .sum()
 }
