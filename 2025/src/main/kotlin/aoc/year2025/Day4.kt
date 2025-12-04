c181f5292d7c0e566f095f9f20614aa4903ff51b
Paul Woitaschek <woitaschek@gmail.com>
2025-12-04 15:35:30 +0100

    Simplify and optimize part 2

--- 2025/src/main/kotlin/aoc/year2025/Day4.kt
+++ 2025/src/main/kotlin/aoc/year2025/Day4.kt
@@ -9,34 +9,31 @@ object Day4 : Puzzle<Int, Int>(day = 4) {
 
   override fun solvePart1(input: String): Int {
     val grid = parse(input)
-    return grid.filterValues { it }
-      .count {
-        isRemovable(grid, it.key)
+    return grid
+      .count { (point, isPaper) ->
+        isPaper && isRemovable(grid, point)
       }
   }
 
   override fun solvePart2(input: String): Int {
     val grid = parse(input).toMutableMap()
-
     var removed = 0
     while (true) {
-      val toRemove = grid.firstNotNullOfOrNull {
-        if (it.value && isRemovable(grid, it.key)) {
-          it.key
-        } else {
-          null
+      val removablePaperRolls = removablePaperRolls(grid)
+      if (removablePaperRolls.isEmpty()) {
+        return removed
+      } else {
+        removed += removablePaperRolls.size
+        removablePaperRolls.forEach {
+          grid[it] = false
         }
       }
-      if (toRemove != null) {
-        grid[toRemove] = false
-        removed++
-      } else {
-        break
-      }
     }
-
-    return removed
   }
+
+  private fun removablePaperRolls(grid: Grid<Boolean>): Set<Point> = grid.filter { (point, isPaper) ->
+    isPaper && isRemovable(grid, point)
+  }.keys
 
   private fun parse(input: String): Grid<Boolean> = grid(input).mapValues { (_, char) ->
     when (char) {
@@ -49,10 +46,7 @@ object Day4 : Puzzle<Int, Int>(day = 4) {
   private fun isRemovable(
     grid: Grid<Boolean>,
     point: Point,
-  ): Boolean {
-    val adjacent = point.adjacent()
-    return adjacent.count {
-      grid[it] == true
-    } < 4
-  }
+  ): Boolean = point.adjacent().count {
+    grid[it] == true
+  } < 4
 }
